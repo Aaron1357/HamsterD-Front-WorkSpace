4e8cd828808708460f8cbf6684fb80c0549cbd3e
KooKwanWon <rksjsl@naver.com>
2023-10-24 14:59:26 +0900

    1

--- hamsterd-front-workspace/src/api/studygroup.js
+++ hamsterd-front-workspace/src/api/studygroup.js
@@ -1,31 +1,38 @@
 import axios from "axios";
+
 const instance = axios.create({
   baseURL: "http://localhost:8080/hamsterd/",
 });
+
 // 스터디그룹 등록
 export const addStudyGroup = async (data) => {
   console.log(data);
   return await instance.post("studygroup", data);
 };
+
 // 스터디그룹 목록 받아오기
 export const getStudyGroupList = async () => {
   return await instance.get("studygroup");
 };
+
 // 스터디그룹의 방장 목록 받아오기
 export const getManagerList = async () => {
   console.log("dddd");
   return await instance.get("member/manager");
 };
+
 // 그룹넘버를 기본키로 가지고 있는 스터디그룹
 export const viewStudyGroup = async (groupNo) => {
   const result = await instance.get(`studygroup/${groupNo}`);
   return result.data;
 };
+
 // 그룹넘버를 기본키로 가지고 있는 스터디그룹에 속한 멤버 리스트
 export const viewMemberList = async (groupNo) => {
   const result = await instance.get(`studygroup/${groupNo}/member`);
   return result.data;
 };
+
 // 그룹넘버를 기본키로 가지고 있는 스터디그룹 방장 멤버
 export const viewManager = async (groupNo) => {
   // console.log("그룹넘버" + groupNo);
@@ -39,3 +46,5 @@ export const showMember = async (id) => {
   const result = await instance.get(`member/${id}`);
   return result.data;
 };
+
+// 스터디그룹 가입
